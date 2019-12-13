b9ca0d9e68ba695f3123667e018d8c3e3fda66e8
selena0391 <39070246+selena0391@users.noreply.github.com>
2019-12-12 23:18:51 -0500

    purple

--- client/src/App.js
+++ client/src/App.js
@@ -1,99 +1,101 @@
 import React, { Component } from 'react';
-
 import {
   handleInput,
-  connectToRoom,
   connectToChatkit,
-  sendMessage,
+  connectToRoom,
   sendDM,
+  sendMessage,
+  updateLanguage,
 } from './methods';
+
 import Dialog from './components/Dialog';
 import RoomList from './components/RoomList';
-import ChatSession from './components/ChatSession';
 import RoomUsers from './components/RoomUsers';
+import ChatSession from './components/ChatSession';
 
-    import 'skeleton-css/css/normalize.css';
-    import 'skeleton-css/css/skeleton.css';
-    import './App.css';
+import 'skeleton-css/css/normalize.css';
+import 'skeleton-css/css/skeleton.css';
+import './App.css';
 
-    class App extends Component {
-      constructor() {
-        super();
-        this.state = {
-          userId: '',
-          showLogin: true,
-          isLoading: false,
-          currentUser: null,
-          currentRoom: null,
-          rooms: [],
-          roomUsers: [],
-          roomName: null,
-          messages: [],
-          newMessage: '',
-        };
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userId: '',
+      showLogin: true,
+      isLoading: false,
+      currentUser: null,
+      currentRoom: null,
+      rooms: [],
+      roomUsers: [],
+      roomName: null,
+      messages: [],
+      newMessage: '',
+      language: 'en',
+    };
 
-        this.handleInput = handleInput.bind(this);
-        this.connectToChatkit = connectToChatkit.bind(this);
-        this.connectToRoom = connectToRoom.bind(this);
-        this.sendMessage = sendMessage.bind(this);
-        this.sendDM = sendDM.bind(this);
+    this.handleInput = handleInput.bind(this);
+    this.connectToChatkit = connectToChatkit.bind(this);
+    this.connectToRoom = connectToRoom.bind(this);
+    this.sendDM = sendDM.bind(this);
+    this.sendMessage = sendMessage.bind(this);
+    this.updateLanguage = updateLanguage.bind(this);
+  }
 
-      }
+  render() {
+    const {
+      userId,
+      showLogin,
+      rooms,
+      currentRoom,
+      currentUser,
+      messages,
+      newMessage,
+      roomUsers,
+      roomName,
+      language,
+    } = this.state;
 
-      render() {
-        const {
-          userId,
-          showLogin,
-          rooms,
-          currentRoom,
-          currentUser,
-          messages,
-          newMessage,
-          roomUsers,
-          roomName,
-        } = this.state;
-
-        return (
-          <div className="App">
-            <aside className="sidebar left-sidebar">
-            {currentUser ? (
-                <div className="user-profile">
-                  <span className="username">{currentUser.name}</span>
-                  <span className="user-id">{`@${currentUser.id}`}</span>
-                </div>
-              ) : null}
-
-            {currentRoom ? (
-                <RoomList
-                  rooms={rooms}
-                  currentRoom={currentRoom}
-                  connectToRoom={this.connectToRoom}
-                  currentUser={currentUser}
-                />
-              ) : null}
-            </aside>
-            <section className="chat-screen">
-              <header className="chat-header">
-              {currentRoom ? <h3>{roomName}</h3> : null}
-              </header>
-              <ul className="chat-messages">
-              <ChatSession messages={messages} />
-              </ul>
-              <footer className="chat-footer">
-                <form onSubmit={this.sendMessage} className="message-form">
-                  <input
-                    type="text"
-                    value={newMessage}
-                    name="newMessage"
-                    className="message-input"
-                    placeholder="Type your message and hit ENTER to send"
-                    onChange={this.handleInput}
-                  />
-                </form>
-              </footer>
-            </section>
-            <aside className="sidebar right-sidebar">
-            {showLogin ? (
+    return (
+      <div className="App">
+        <aside className="sidebar left-sidebar">
+          {currentUser ? (
+            <div className="user-profile">
+              <span className="username">{currentUser.name}</span>
+              <span className="user-id">{`@${currentUser.id}`}</span>
+            </div>
+          ) : null}
+          {currentRoom ? (
+            <RoomList
+              rooms={rooms}
+              currentRoom={currentRoom}
+              connectToRoom={this.connectToRoom}
+              currentUser={currentUser}
+            />
+          ) : null}
+        </aside>
+        <section className="chat-screen">
+          <header className="chat-header">
+            {currentRoom ? <h3>{roomName}</h3> : null}
+          </header>
+          <ul className="chat-messages">
+            <ChatSession messages={messages} />
+          </ul>
+          <footer className="chat-footer">
+            <form onSubmit={this.sendMessage} className="message-form">
+              <input
+                type="text"
+                value={newMessage}
+                name="newMessage"
+                className="message-input"
+                placeholder="Type your message and hit ENTER to send"
+                onChange={this.handleInput}
+              />
+            </form>
+          </footer>
+        </section>
+        <aside className="sidebar right-sidebar">
+        {showLogin ? (
                 <Dialog
                   userId={userId}
                   handleInput={this.handleInput}
@@ -101,17 +103,33 @@ import RoomUsers from './components/RoomUsers';
                 />
               ) : null}
 
-            {currentRoom ? (
-                <RoomUsers
-                  currentUser={currentUser}
-                  sendDM={this.sendDM}
-                  roomUsers={roomUsers}
-                />
-              ) : null}
-            </aside>
-          </div>
-        );
-      }
+          {currentRoom ? (
+            <RoomUsers
+              currentUser={currentUser}
+              sendDM={this.sendDM}
+              roomUsers={roomUsers}
+            />
+          ) : null}
+          {currentRoom ? (
+            <select
+              id="language"
+              className="language"
+              name="language"
+              value={language}
+              onChange={this.updateLanguage}
+            >
+              <option value="en">English</option>
+              <option value="fr">French</option>
+              <option value="es">Spanish</option>
+              <option value="de">German</option>
+              <option value="ru">Russian</option>
+            </select>
+          ) : null}
+        </aside>
+        
+      </div>
+    );
   }
+}
 
-    export default App;
+export default App;
--- client/src/methods.js
+++ client/src/methods.js
@@ -1,6 +1,22 @@
 import Chatkit from '@pusher/chatkit-client';
 import axios from 'axios';
 
+function sendMessage(event) {
+  event.preventDefault();
+  const { newMessage, currentUser, currentRoom } = this.state;
+
+  if (newMessage.trim() === '') return;
+
+  currentUser.sendMessage({
+    text: newMessage,
+    roomId: `${currentRoom.id}`,
+  });
+
+  this.setState({
+    newMessage: '',
+  });
+}
+
 function handleInput(event) {
   const { value, name } = event.target;
 
@@ -9,52 +25,50 @@ function handleInput(event) {
   });
 }
 
-function connectToRoom(id = 'cce93be1-ae14-44a9-b6f7-29fed8921869') {
-    const { currentUser } = this.state;
+function connectToRoom(id = 'ce85a77e-4ed9-479e-a4cf-40410361796f') {
+  const { currentUser } = this.state;
 
-    this.setState({
-      messages: [],
-    });
+  this.setState({
+    messages: [],
+  });
 
-    return currentUser
-      .subscribeToRoom({
-        roomId: `${id}`,
-        messageLimit: 100,
-        hooks: {
-            onMessage: message => {
-                this.setState({
-                  messages: [...this.state.messages, message],
-                });
+  return currentUser
+    .subscribeToRoom({
+      roomId: `${id}`,
+      messageLimit: 100,
+      hooks: {
+        onMessage: message => {
+          translateText.call(this, message);
         },
         onPresenceChanged: () => {
-            const { currentRoom } = this.state;
-            this.setState({
-              roomUsers: currentRoom.users.sort(a => {
-                if (a.presence.state === 'online') return -1;
+          const { currentRoom } = this.state;
+          this.setState({
+            roomUsers: currentRoom.users.sort(a => {
+              if (a.presence.state === 'online') return -1;
 
-                return 1;
-              }),
-            });
-          },
+              return 1;
+            }),
+          });
         },
-      })
-      .then(currentRoom => {
-        const roomName =
-          currentRoom.customData && currentRoom.customData.isDirectMessage
-            ? currentRoom.customData.userIds.filter(
-                id => id !== currentUser.id
-              )[0]
-            : currentRoom.name;
+      },
+    })
+    .then(currentRoom => {
+      const roomName =
+        currentRoom.customData && currentRoom.customData.isDirectMessage
+          ? currentRoom.customData.userIds.filter(
+              id => id !== currentUser.id
+            )[0]
+          : currentRoom.name;
 
-        this.setState({
-          currentRoom,
-          roomUsers: currentRoom.users,
-          rooms: currentUser.rooms,
-          roomName,
-        });
-      })
-      .catch(console.error);
-  }
+      this.setState({
+        currentRoom,
+        roomUsers: currentRoom.users,
+        rooms: currentUser.rooms,
+        roomName,
+      });
+    })
+    .catch(console.error);
+}
 
 function connectToChatkit(event) {
   event.preventDefault();
@@ -66,6 +80,10 @@ function connectToChatkit(event) {
     return;
   }
 
+  this.setState({
+    isLoading: true,
+  });
+
   axios
     .post('http://localhost:5200/users', { userId })
     .then(() => {
@@ -74,7 +92,7 @@ function connectToChatkit(event) {
       });
 
       const chatManager = new Chatkit.ChatManager({
-        instanceLocator: 'v1:us1:3dcd39c4-6265-4396-86e1-991370549997',
+        instanceLocator: 'v1:us1:93258d08-4de7-4aef-b00d-41d7de23d653',
         userId,
         tokenProvider,
       });
@@ -93,6 +111,7 @@ function connectToChatkit(event) {
             {
               currentUser,
               showLogin: false,
+              isLoading: false,
               rooms: currentUser.rooms,
             },
             () => connectToRoom.call(this)
@@ -102,60 +121,96 @@ function connectToChatkit(event) {
     .catch(console.error);
 }
 
-function sendMessage(event) {
-    event.preventDefault();
-    const { newMessage, currentUser, currentRoom } = this.state;
-
-    if (newMessage.trim() === '') return;
-
-    currentUser.sendMessage({
-      text: newMessage,
-      roomId: `${currentRoom.id}`,
-    });
-
-    this.setState({
-      newMessage: '',
-    });
-}
-
 function createPrivateRoom(id) {
-    const { currentUser, rooms } = this.state;
-    const roomName = `${currentUser.id}_${id}`;
+  const { currentUser, rooms } = this.state;
+  const roomName = `${currentUser.id}_${id}`;
 
-    const isPrivateChatCreated = rooms.filter(room => {
-      if (room.customData && room.customData.isDirectMessage) {
-        const arr = [currentUser.id, id];
-        const { userIds } = room.customData;
+  const isPrivateChatCreated = rooms.filter(room => {
+    if (room.customData && room.customData.isDirectMessage) {
+      const arr = [currentUser.id, id];
+      const { userIds } = room.customData;
 
-        if (arr.sort().join('') === userIds.sort().join('')) {
-          return {
-            room,
-          };
-        }
+      if (arr.sort().join('') === userIds.sort().join('')) {
+        return {
+          room,
+        };
       }
-
-      return false;
-    });
-
-    if (isPrivateChatCreated.length > 0) {
-      return Promise.resolve(isPrivateChatCreated[0]);
     }
 
-    return currentUser.createRoom({
-      name: `${roomName}`,
-      private: true,
-      addUserIds: [`${id}`],
-      customData: {
-        isDirectMessage: true,
-        userIds: [currentUser.id, id],
-      },
-    });
+    return false;
+  });
+
+  if (isPrivateChatCreated.length > 0) {
+    return Promise.resolve(isPrivateChatCreated[0]);
   }
 
-  function sendDM(id) {
-    createPrivateRoom.call(this, id).then(room => {
-      connectToRoom.call(this, room.id);
-    });
-  }
+  return currentUser.createRoom({
+    name: `${roomName}`,
+    private: true,
+    addUserIds: [`${id}`],
+    customData: {
+      isDirectMessage: true,
+      userIds: [currentUser.id, id],
+    },
+  });
+}
 
-export { handleInput, connectToRoom, connectToChatkit, sendMessage, sendDM }
+function sendDM(id) {
+  createPrivateRoom.call(this, id).then(room => {
+    connectToRoom.call(this, room.id);
+  });
+}
+
+function updateLanguage(event) {
+  const { value } = event.target;
+  const { messages } = this.state;
+  this.setState(
+    {
+      language: value,
+    },
+    () => {
+      messages.forEach(message => {
+        translateText.call(this, message);
+      });
+    }
+  );
+}
+
+function translateText(message) {
+  const { language, messages } = this.state;
+  const { text, id } = message;
+  axios
+    .post('http://localhost:5200/translate', {
+      text,
+      lang: language,
+    })
+    .then(response => {
+      const index = messages.findIndex(item => item.id === id);
+      const msg = {
+        ...message,
+        text: response.data.TranslatedText,
+      };
+
+      if (index !== -1) {
+        messages.splice(index, 1, msg);
+      } else {
+        messages.push(msg);
+      }
+
+      this.setState({
+        messages: messages.sort((a, b) => {
+          return new Date(a.createdAt) - new Date(b.createdAt);
+        }),
+      });
+    })
+    .catch(console.error);
+}
+
+export {
+  sendMessage,
+  handleInput,
+  connectToRoom,
+  connectToChatkit,
+  sendDM,
+  updateLanguage,
+};
